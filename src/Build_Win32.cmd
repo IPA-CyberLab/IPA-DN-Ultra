b1446b2273d08729a1b04184f4b779e1455d21c9
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-07-30 20:32:26 +0900

    win32 build action

---
+++ src/Build_Win32.cmd
@@ -0,0 +1,54 @@
+SETLOCAL
+SET BATCH_FILE_NAME=%0
+SET BATCH_DIR_NAME=%0\..
+
+for /f "usebackq tokens=*" %%i in (`"%BATCH_DIR_NAME%\BuildFiles\Utility\vswhere.exe" -version [16.0^,17.0^) -sort -requires Microsoft.Component.MSBuild -find Common7\Tools\VsDevCmd.bat`) do (
+    if exist "%%i" (
+        call "%%i"
+    )
+)
+
+echo on
+
+
+cd /d %BATCH_DIR_NAME%
+
+
+msbuild /target:Clean /property:Configuration=Debug /property:Platform=x86 IPA-DN-Ultra-VS2019.sln
+IF ERRORLEVEL 1 GOTO LABEL_ERROR
+
+msbuild /verbosity:detailed /target:Rebuild /maxcpucount:8 /property:Configuration=Debug /property:Platform=x86 IPA-DN-Ultra-VS2019.sln
+IF ERRORLEVEL 1 GOTO LABEL_ERROR
+
+
+
+msbuild /target:Clean /property:Configuration=Debug /property:Platform=x64 IPA-DN-Ultra-VS2019.sln
+IF ERRORLEVEL 1 GOTO LABEL_ERROR
+
+msbuild /verbosity:detailed /target:Rebuild /maxcpucount:8 /property:Configuration=Debug /property:Platform=x64 IPA-DN-Ultra-VS2019.sln
+IF ERRORLEVEL 1 GOTO LABEL_ERROR
+
+
+
+msbuild /target:Clean /property:Configuration=Release /property:Platform=x86 IPA-DN-Ultra-VS2019.sln
+IF ERRORLEVEL 1 GOTO LABEL_ERROR
+
+msbuild /verbosity:detailed /target:Rebuild /maxcpucount:8 /property:Configuration=Release /property:Platform=x86 IPA-DN-Ultra-VS2019.sln
+IF ERRORLEVEL 1 GOTO LABEL_ERROR
+
+
+
+msbuild /target:Clean /property:Configuration=Release /property:Platform=x64 IPA-DN-Ultra-VS2019.sln
+IF ERRORLEVEL 1 GOTO LABEL_ERROR
+
+msbuild /verbosity:detailed /target:Rebuild /maxcpucount:8 /property:Configuration=Release /property:Platform=x64 IPA-DN-Ultra-VS2019.sln
+IF ERRORLEVEL 1 GOTO LABEL_ERROR
+
+
+:LABEL_ERROR
+
+
+
+EXIT %ERRORLEVEL%
+
+
